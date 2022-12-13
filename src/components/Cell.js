91f9758e9ed493ee61c19bd0869b6af24a3902c2
VanesMP <vanessa.MP@outlook.fr>
2022-12-13 13:17:04 +0100

    creating components

---
+++ src/components/Cell.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function Cell( { type } ) {
+  return (
+    <div>
+      cell
+    </div>
+  );
+}
+
+export default Cell;
---
+++ src/components/StartButton.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function StartButton({ callback }) {
+  return (
+    <div>
+      Start Game
+    </div>
+  );
+}
+
+export default StartButton;
