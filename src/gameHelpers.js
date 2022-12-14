cb03e684b243d20b7a77ac6089e1293d31f1f78b
VanesMP <vanessa.MP@outlook.fr>
2022-12-14 13:07:49 +0100

    start create 2 hooks - usePlayer - useStage

--- src/gameHelpers.js
+++ src/gameHelpers.js
@@ -1,6 +1,8 @@
 export const STAGE_WIDTH = 12;
 export const STAGE_HEIGHT = 20;
 
+//creating a array ==== tetris grid
+
 export const createStage = () =>
     Array.from(Array(STAGE_HEIGHT), () =>
         new Array(STAGE_WIDTH).fill([0, 'clear'])
